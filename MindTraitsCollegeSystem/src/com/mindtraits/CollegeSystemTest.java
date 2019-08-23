5f57570cf210b74d89a0a92d2e880cc6259ab3c2
Vignesh <vignesh.v@mindtraits.in>
2019-08-23 13:10:05 +0530

    Updated

---
+++ MindTraitsCollegeSystem/src/com/mindtraits/CollegeSystemTest.java
@@ -0,0 +1,63 @@
+package com.mindtraits;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+public class CollegeSystemTest {
+
+	@Test
+	public void testSearchStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchCourse() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+//	public void testAddStudentCollegeSystem() throws ParseException {
+//		CollegeSystem collegeSystem = new CollegeSystem();
+//		Student student = new Student();
+//		student.getName();
+//		collegeSystem.addStudent(collegeSystem);
+//		assertEquals(0,true);
+//		
+//		//fail("Not yet implemented");
+//	}
+
+	@Test
+	public void testRemoveStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddCourseCollegeSystem() {
+		CollegeSystem collegeSystem = new CollegeSystem();
+		Course course = new Course();
+		course.getName();
+		collegeSystem.addCourse(collegeSystem);
+		assertEquals(0,true);
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCourse() {
+		fail("Not yet implemented");
+	}
+
+}
