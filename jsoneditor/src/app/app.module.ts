6136d4e515ac9551e6428c881d3ec31e360ae636
manisnow <manisnow@gamil.com>
2020-03-15 11:02:38 -0400

    added json editor in java training

---
+++ jsoneditor/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './core/app.routing.module';
+import { AppComponent } from './app.component';
+import { JsoneditorComponent } from './jsoneditor/jsoneditor.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './core/material-module';
+import { XmlviewerComponent } from './xmlviewer/xmlviewer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JsoneditorComponent,
+    XmlviewerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ jsoneditor/src/app/xmlviewer/xmlviewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xmlviewer',
+  templateUrl: './xmlviewer.component.html',
+  styleUrls: ['./xmlviewer.component.css']
+})
+export class XmlviewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
