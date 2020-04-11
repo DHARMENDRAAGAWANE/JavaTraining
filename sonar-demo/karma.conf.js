f5b8c4a2db0b8f64609d38888f76271cbe48aacf
VickyGupta <vicky.gupta@mindtraits.com>
2020-04-11 13:49:36 +0530

    commited for configur headless chrome for testing

--- sonar-demo/karma.conf.js
+++ sonar-demo/karma.conf.js
@@ -26,7 +26,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS','Chrome','ChromeHeadless'],
+    //browsers: ['PhantomJS','Chrome','ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
